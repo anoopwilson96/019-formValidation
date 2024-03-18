1fd1042a7a0f3c8aac63c1db407962a85471ba0a
Anoop Wilson <anoopwilson33@yahoo.com>
2024-03-18 16:54:52 +0530

    added js file.

---
+++ script.js
@@ -0,0 +1,22 @@
+const nameInputJS = document.getElementById('nameInput')
+const nameErrorJS = document.getElementById('nameError')
+
+const emailInputJS= document.getElementById('emailInput')
+const emailErrorJS=document.getElementById('emailError')
+
+const passwordInput1=document.getSelection('passwordInput1')
+const passwordError1=document.getSelection('passwordError1')
+
+const passwordInput2JS=document.getSelection('passwordInput2')
+const passwordError2JS=document.getSelection('passwordError2')
+
+const submitButtonJS = document.getElementById('submitButton')
+
+/* 
+<span id="nameError">Name cannot be blank.</span>
+<span id="emailError">Email address cannot be blank.</span>
+<span id="passwordError1">Password must contain at least one uppercase letter, one lowercase letter, one special character, one digit, and be at least 8 characters long.</span>
+*/
+
+
+
